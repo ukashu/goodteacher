36f2ca77d6d3a359495c2230af05ff4c2e0f1fe7
Lukasz Filipiuk <lukaszfilipiuk123@gmail.com>
2023-05-16 12:09:21 +0200

    created Login page

---
+++ app/components/CustomInput.tsx
@@ -0,0 +1,13 @@
+import { View, Text, TextInput, TextInputProps } from 'react-native';
+import React from 'react';
+import tw from 'twrnc';
+
+export default function CustomInput(props: TextInputProps) {
+  const { ...rest } = props
+
+  return (
+    <View style={tw`border-b border-gray-500 my-0 px-8 w-100% items-center`}>
+      <TextInput {...rest}/>
+    </View>
+  )
+}
--- app/screens/Login.tsx
+++ app/screens/Login.tsx
@@ -1,4 +1,5 @@
-import { View, StyleSheet, TextInput, Button } from 'react-native';
+import { View, Text, StyleSheet, TextInput, TouchableOpacity } from 'react-native';
+import { Dimensions } from 'react-native';
 import React from 'react';
 import { useAuth } from '../context/AuthContext';
 import { NativeStackNavigationProp } from '@react-navigation/native-stack';
@@ -6,6 +7,7 @@ import { RootStackParamList } from '../../App';
 import tw from 'twrnc';
 import Logo from '../../assets/logo.svg';
 import Background from '../../assets/background.svg';
+import CustomInput from '../components/CustomInput'
 
 type LoginScreenNavigationProp = NativeStackNavigationProp<
   RootStackParamList,
@@ -29,14 +31,29 @@ export default function Login({ navigation }: Props) {
   }
 
   return (
-    <View>
+    <View style={{
+      position: 'absolute',
+      left: 0,
+      top: 0,
+      width: Dimensions.get('window').width,
+      height: Dimensions.get('window').height,
+    }}>
       <Background width="100%" height="110%" preserveAspectRatio="none" style={tw`absolute z-1`}/>
       <View style={tw`z-2 items-center justify-between m-auto h-100% w-100%`}>
         <Logo width={512} height={296} />
-        <View>
-          <TextInput placeholder="Email" onChangeText={(text: string) => setEmail(text)} value={email}/>
-          <TextInput placeholder="Password" secureTextEntry={true} onChangeText={(password: string) => setPassword(password)} value={password}/>
-          <Button onPress={login} title="Sign In"/>
+        <View style={tw`w-100% h-50% items-center justify-around px-12`}>
+          <View style={tw`w-100% h-75% justify-around items-center`}>
+            <CustomInput placeholder="email" onChangeText={(text: string) => setEmail(text)} value={email} autoCapitalize='none'/>
+            <CustomInput placeholder="password" secureTextEntry={true} onChangeText={(password: string) => setPassword(password)} value={password} autoCapitalize='none'/>
+            <View style={tw`w-100% flex-row justify-between`}>
+              <Text></Text>
+              <TouchableOpacity style={tw`px-4 py-2 rounded bg-blue-500`} onPress={login}><Text style={tw`text-white`}>sign in</Text></TouchableOpacity>
+            </View>
+          </View>
+          <View style={tw`w-100% flex-row justify-between items-center`}>
+              <Text style={tw`text-white`} >{"Don't have an account? "}</Text>
+              <TouchableOpacity style={tw`px-4 py-2 rounded bg-blue-500 self-end`} onPress={() => navigation.navigate('Register')}><Text style={tw`text-white`}>sign up</Text></TouchableOpacity>
+            </View>
         </View>
       </View>
     </View>
