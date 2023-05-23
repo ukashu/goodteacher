a2ccc72dab264010428aa5698bebedec84a42e04
Lukasz Filipiuk <lukaszfilipiuk123@gmail.com>
2023-05-23 16:45:55 +0200

    changed loading screen

--- app/screens/Loading.tsx
+++ app/screens/Loading.tsx
@@ -1,11 +1,22 @@
-import { View, Text } from 'react-native';
+import { View, Text, ActivityIndicator } from 'react-native';
+import { Dimensions } from 'react-native';
 import React from 'react';
 import tw from 'twrnc';
+import Background from '../../assets/background.svg';
 
 export default function Loading() {
   return (
-    <View style={tw` h-100% bg-red-600`}>
-      <Text>Loading...</Text>
+    <View style={{
+      position: 'absolute',
+      left: 0,
+      top: 0,
+      display: 'flex',
+      justifyContent: 'center',
+      width: Dimensions.get('window').width,
+      height: Dimensions.get('window').height,
+    }}>
+      <Background width="100%" height="110%" preserveAspectRatio="none" style={tw`absolute z-1`}/>
+      <ActivityIndicator size="large" color="red" style={tw`absolute z-2 self-center justify-around my-auto`}/>
     </View>
   )
 }
