53ace73fd2016322eb7460592f024776b3b0e340
Lukasz Filipiuk <lukaszfilipiuk123@gmail.com>
2023-05-30 15:30:21 +0200

    changed back login error handling

--- app/screens/Login.tsx
+++ app/screens/Login.tsx
@@ -25,12 +25,8 @@ export default function Login({ navigation }: Props) {
 
   const login = async () => {
     const result = await onLogin!({email, password})
-    if (result.msg) {
+    if (result && result.error) {
       alert(result.msg)
-    } else if (result.data.message) {
-      alert(result.data.message)
-    } else {
-      alert('Something went wrong, try again')
     }
   }
 
