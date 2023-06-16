708eec7bf7016d0fc99dd3cdd4934a43436d2d9d
Lukasz Filipiuk <lukaszfilipiuk123@gmail.com>
2023-06-16 13:49:17 +0200

    added translation to zod errors

--- app/utils/schemas.ts
+++ app/utils/schemas.ts
@@ -1,19 +1,20 @@
 import z from 'zod'
+import i18next from 'i18next';
 
 export const classSchema = z.object({
   name: z.string({
-    required_error: 'Name is required'
-  }).min(1, 'Name must be at least 1 character long').max(25, 'Name must be less than 25 characters long'),
+    required_error: i18next.t('zodErrors.Name is required')
+  }).min(1, i18next.t('zodErrors.Name must be at least 1 character long')).max(25, i18next.t('zodErrors.Name must be less than 25 characters long')),
 })
 
 export const studentSchema = z.object({
-  email: z.string().email("Not a valid email"),
-  alias: z.string().min(1, "Alias must be at least 1 character long").max(25, "Alias must be at most 25 characters long")
+  email: z.string().email(i18next.t("zodErrors.Not a valid email")),
+  alias: z.string().min(1, i18next.t("zodErrors.Alias must be at least 1 character long")).max(25, i18next.t("zodErrors.Alias must be less than 25 characters long"))
 })
 
 export const taskSchema = z.object({
-  title: z.string().min(1, "Title must be at least 1 character long").max(25, "Title must be at most 25 characters long"),
-  description: z.string().min(1, "Description must be at least 1 character long").max(140, "Description must be at most 140 characters long")
+  title: z.string().min(1, i18next.t("zodErrors.Title must be at least 1 character long")).max(25, i18next.t("zodErrors.Title must be less than 25 characters long")),
+  description: z.string().min(1, i18next.t("zodErrors.Description must be at least 1 character long")).max(140, i18next.t("zodErrors.Description must be less than 140 characters long"))
 })
 
 const userTypes = z.enum(['STUDENT', 'TEACHER'])
@@ -23,31 +24,31 @@ export type UserTypes = z.infer<typeof userTypes>
 export const registerUserSchema = z.object({
   body: z.object({
     name: z.string({
-      required_error: 'Name is required'
-    }).min(1, 'Name must be at least 1 character long').max(20, 'Name must be less than 20 characters long'),
+      required_error: i18next.t('zodErrors.Name is required')
+    }).min(1, i18next.t('zodErrors.Name must be at least 1 character long')).max(20, i18next.t('zodErrors.Name must be less than 20 characters long')),
     email: z.string({
-      required_error: 'Email is required'
-    }).email("Not a valid email"),
+      required_error: i18next.t('zodErrors.Email is required')
+    }).email(i18next.t("zodErrors.Not a valid email")),
     password: z.string({
-      required_error: 'Password is required'
-    }).min(6, 'Password must be at least 6 characters long').max(100, 'Password must be less than 100 characters long'),
+      required_error: i18next.t('zodErrors.Password is required')
+    }).min(6, i18next.t('zodErrors.Password must be at least 6 characters long')).max(100, i18next.t('zodErrors.Password must be less than 100 characters long')),
     passwordConfirmation: z.string({
-      required_error: 'Password confirmation is required'
+      required_error: i18next.t('zodErrors.Password confirmation is required')
     }),
     type: userTypes,
   }).refine(data => data.password === data.passwordConfirmation, {
-    message: 'Passwords do not match'
+    message: i18next.t('zodErrors.Passwords do not match')
   })
 })
 
 export const loginUserSchema = z.object({
   body: z.object({
     email: z.string({
-      required_error: 'Email is required'
-    }).email("Not a valid email"),
+      required_error: i18next.t('zodErrors.Email is required')
+    }).email(i18next.t("zodErrors.Not a valid email")),
     password: z.string({
-      required_error: 'Password is required'
-    }).min(6, 'Password must be at least 6 characters long').max(100, 'Password must be less than 100 characters long'),
+      required_error: i18next.t('zodErrors.Password is required')
+    }).min(6, i18next.t('zodErrors.Password must be at least 6 characters long')).max(100, i18next.t('zodErrors.Password must be less than 100 characters long')),
   })
 })
 
